d206bbadd07d5fa0ad8628b0a99db5fbacce04da
j-parthiv <parthivjani9@gmail.com>
2024-05-07 21:55:00 +0200

    feat: create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+
 const Home = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">Smart Shopping Starts Here:</p>
+            <h1 className="head-text">
+              Unleash the power of{" "}
+              <span className="text-primary">DealFinder</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-10">
+          {["Apple Phone", "Book", "Sneakers"].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
