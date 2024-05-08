298ba445ba678e0fad5f39e848dfbfa6e84ae046
j-parthiv <parthivjani9@gmail.com>
2024-05-08 19:45:32 +0200

    refactor: modify next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+};
