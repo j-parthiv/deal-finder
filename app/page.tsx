b3a70a37bb85954ff2c53b94ead22f0ab1c91148
j-parthiv <parthivjani9@gmail.com>
2024-05-13 19:49:29 +0200

    feat: list products on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
-
-const Home = () => {
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+const Home = async () => {
+  const allProducts = await getAllProducts();
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -25,8 +28,8 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-10">
-          {["Apple Phone", "Book", "Sneakers"].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
