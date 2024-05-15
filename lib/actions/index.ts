73e61de9d7f2c5df699c6466729cef592cbd8cfc
j-parthiv <parthivjani9@gmail.com>
2024-05-15 19:00:20 +0200

    feat: create a function to add user email to product

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,6 +4,8 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string){
      if(!productUrl) return;
@@ -65,5 +67,46 @@ export async function getAllProducts(){
         return products;
     } catch (error: any) {
         console.log(error);
+    }
+}
+ 
+export async function getSimilarProducts(productId: string){
+try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+        _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+
+} catch (error: any) {
+    console.log(error);
+}
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+      const product = await Product.findById(productId);
+  
+      if(!product) return;
+  
+      const userExists = product.users.some((user: User) => user.email === userEmail);
+  
+      if(!userExists) {
+        product.users.push({ email: userEmail });
+  
+        await product.save();
+  
+        const emailContent = await generateEmailBody(product, "WELCOME");
+  
+        await sendEmail(emailContent, [userEmail]);
+      }
+    } catch (error) {
+      console.log(error);
     }
 }
